f1ba7e600203f8cfa52f49b7a63618f01d1423d3
Ivan Zarea <zarea.ion@gmail.com>
2013-08-29 15:28:23 +0000

    First commit, privetiki bumbu!

---
+++ app.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var app = express();
+
+app.set('views', __dirname + '/views');
+app.engine('html', require('ejs').renderFile);
+
+app.get('/', function(req, res){
+  res.render('index.html');
+});
+
+app.listen(80);
